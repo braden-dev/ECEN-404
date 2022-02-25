b5e82fe4b7dbd809ed711e5e79e3631086f930b9
Braden <bkd@tamu.edu>
2022-02-25 13:31:50 -0600

    added icp.py and some 3D models to compare

---
+++ 3DModelComparisions/icp.py
@@ -0,0 +1,7 @@
+import open3d as o3d
+
+# pcd = o3d.io.read_point_cloud("~/3DModels/Octagon/real-octagon.ply")
+# o3d.io.write_point_cloud("~/3DModels/Octagon/RealOct.pcd", pcd)
+
+pcd = o3d.io.read_point_cloud("reconstructed-octagon.ply")
+o3d.io.write_point_cloud("ReconOct.pcd", pcd)
