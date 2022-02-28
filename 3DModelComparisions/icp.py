ae3b7f355eb11f9018570650e752982f05fd7468
Braden <bkd@tamu.edu>
2022-02-28 10:34:18 -0600

    added global registration and ICP plus testing '.pcd's

--- 3DModelComparisions/icp.py
+++ 3DModelComparisions/icp.py
@@ -3,5 +3,8 @@ import open3d as o3d
 # pcd = o3d.io.read_point_cloud("~/3DModels/Octagon/real-octagon.ply")
 # o3d.io.write_point_cloud("~/3DModels/Octagon/RealOct.pcd", pcd)
 
+pcd = o3d.io.read_point_cloud("simplified-resized-real-octagon.ply")
+o3d.io.write_point_cloud("SimplifiedResizedRealOctagon.pcd", pcd)
+
 pcd = o3d.io.read_point_cloud("reconstructed-octagon.ply")
-o3d.io.write_point_cloud("ReconOct.pcd", pcd)
+o3d.io.write_point_cloud("ReconstructedOctagon.pcd", pcd)
